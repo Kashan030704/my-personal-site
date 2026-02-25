dd57f02dd59186afed882310f814f2f8d3c54d90
Kashan <kashan21khan@gmail.com>
2026-02-25 04:33:52 -0500

    Added required field and a non-functional send email button

--- src/app/Contact/components/ContactModal.tsx
+++ src/app/Contact/components/ContactModal.tsx
@@ -1,9 +1,22 @@
 export default function ContactModal(){
     return (
-        <div className="p-4 mt-15 flex justify-center h-140 w-254 rounded-lg bg-white border-gray-300 shadow-sm">
-            <div className="bg-slate-200 rounded-lg shadow-sm border shadow-black h-9">
-                <h1 className="text-2xl text-black font-bold">Send Direct Email</h1>
-                <hr className="border-green-300 border-2"></hr>
+        <div className="p-4 mt-15 flex flex-col justify-between h-140 w-254 rounded-lg bg-white border-gray-300 shadow-sm">
+            {/*User Information */}
+            <div className="bg-yellow-200 border-lg p-2">
+                <h1 className="font-bold text-black">Important: user information must be field</h1>
+            </div>
+
+            <div> {/*Send Mail Button*/}
+                <div className="flex justify-center bg-slate-200 rounded-lg border shadow-sm shadow-black h-9">
+                    <h1 className="text-2xl text-black font-bold">
+                        <button type="submit" className="rounded-lg hover:text-white hover:bg-green-300 w-247 h-10 mb-1">
+                            Send Direct Email
+                        </button>
+                    </h1>
+                </div>
+                <div className="flex justify-center">
+                    <hr className="border-green-300 border-2 w-200"></hr>
+                </div>
             </div>
         </div>
     );
